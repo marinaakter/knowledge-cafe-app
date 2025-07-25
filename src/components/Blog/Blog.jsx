95ce5b06cbd39cb607c57507d0410b9311a5cf54
Marina <marinaakter878@gmail.com>
2025-07-25 16:27:54 +0600

    Add react-icons dependency and integrate bookmark icon in Blog component

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { IoMdBookmark } from "react-icons/io";
 const Blog = ({ blog }) => {
   const {
     cover,
@@ -34,18 +35,11 @@ const Blog = ({ blog }) => {
           </div>
         </div>
         {/* Reading time div */}
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-3">
           <span>{reading_time} min read</span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            x="0px"
-            y="0px"
-            width="30"
-            height="30"
-            viewBox="0 0 24 24"
-          >
-            <path d="M 6 2 C 4.8444444 2 4 2.9666667 4 4 L 4 22.039062 L 12 19.066406 L 20 22.039062 L 20 20.599609 L 20 4 C 20 3.4777778 19.808671 2.9453899 19.431641 2.5683594 C 19.05461 2.1913289 18.522222 2 18 2 L 6 2 z"></path>
-          </svg>
+          <button className="text-2xl">
+            <IoMdBookmark />
+          </button>
         </div>
       </div>
       <h2 className="text-4xl font-bold mb-4">{title}</h2>
